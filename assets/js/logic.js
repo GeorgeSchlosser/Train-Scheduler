a760dd58416f3e04d731f20738ef1608621da2c3
Trash Panda <d.george.schlosser@gmail.com>
2019-04-25 01:49:06 -0400

    functional 'SUBMIT' button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,14 +9,38 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-console.log(database);
 
 // Button to Add Trains
 $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
 
     // Grabs User Input
-    var trnName = $("#train-name-input")
+    var trnName = $("#train-name-input").val().trim();
+    var trnDest = $("#destination-input").val().trim();
+    var trnFirst = moment($("#first-input").val().trim(), "HH:mm").format("X");
+    var trnFreq = $("#frequency-input").val().trim();
 
+    // Create Local/Temporary Object to hold Train Data
+    var newTrn = {
+        name: trnName,
+        destination: trnDest,
+        start: trnFirst,
+        frequency: trnFreq,
+    };
 
+    // Upload Train Data to Database
+    database.ref().push(newTrn);
 
+    // LOG ALL THE THINGS
+    console.log(newTrn);
+    console.log(newTrn.name);
+    console.log(newTrn.destination);
+    console.log(newTrn.start);
+    console.log(newTrn.frequency);
+
+    // Clear Form Text-Boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-input").val("");
+    $("#frequency-input").val("");
 });
