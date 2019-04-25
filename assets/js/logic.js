3975db4a7ad446fb214e3ac927c93bf894a376d3
Trash Panda <d.george.schlosser@gmail.com>
2019-04-25 00:54:47 -0400

    firebase linked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,22 @@
-// GLOBAL VARIABLES
-// ==============================================
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBXE_jCyyxKzd6sCnklhA_QDaC0JVt2wAY",
+    authDomain: "train-scheduler-79eab.firebaseapp.com",
+    databaseURL: "https://train-scheduler-79eab.firebaseio.com",
+    storageBucket: "train-scheduler-79eab.appspot.com"
+};
 
-// FUNCTIONS
-// ==============================================
+firebase.initializeApp(config);
 
-// APPLICATION
-// ==============================================
+var database = firebase.database();
+console.log(database);
+
+// Button to Add Trains
+$("#add-train-btn").on("click", function(event) {
+
+    // Grabs User Input
+    var trnName = $("#train-name-input")
+
+
+
+});
